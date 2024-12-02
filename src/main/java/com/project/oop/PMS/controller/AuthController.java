5412e22c288e6e27dc7444fce357866edb88f7bf
levietducanh99 <ducanhvahung@gmail.com>
2024-12-02 14:03:32 +0700

    delete frontend

--- src/main/java/com/project/oop/PMS/controller/AuthController.java
+++ src/main/java/com/project/oop/PMS/controller/AuthController.java
@@ -1,19 +1,48 @@
 package com.project.oop.PMS.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.project.oop.PMS.dto.ApiResponse;
+import com.project.oop.PMS.dto.LoginRequest;
+import com.project.oop.PMS.dto.RegisterRequest;
+import com.project.oop.PMS.dto.UserResponse;
+import com.project.oop.PMS.entity.User;
+import com.project.oop.PMS.service.UserService;
 
-@Controller
-@RequestMapping("api/auth")
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/auth")
 public class AuthController {
 
-    @GetMapping("/login")
-    public String showLoginPage() {
-        return "login"; // Tên file HTML trong thư mục templates
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@RequestBody RegisterRequest request) {
+        try {
+            User newUser = userService.registerUser(request.getUsername(), request.getPassword());
+            UserResponse userResponse = new UserResponse(newUser.getUserId(), newUser.getUsername());
+            return ResponseEntity.ok(new ApiResponse("User registered successfully", userResponse));
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(new ApiResponse("Error: " + e.getMessage()));
+        }
     }
-    @GetMapping("/register")
-    public String showRegisterPage() {
-        return "register"; // Tên file HTML trong thư mục templates
+
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody LoginRequest request) {
+        Optional<User> user = userService.loginUser(request.getUsername(), request.getPassword());
+        
+        return user.map(u -> {
+            UserResponse userResponse = new UserResponse(u.getUserId(), u.getUsername());
+            return ResponseEntity.ok(new ApiResponse("User login successfully", userResponse));
+        }).orElseGet(() -> {
+            return ResponseEntity.status(401).body(new ApiResponse("Invalid username or password"));
+        });
     }
+
+
 }
