b349ba61d3c54656187926ceaad7de630625d26b
levietducanh99 <ducanhvahung@gmail.com>
2024-12-02 13:53:49 +0700

    allow all source

--- src/main/java/com/project/oop/PMS/config/WebMvcConfig.java
+++ src/main/java/com/project/oop/PMS/config/WebMvcConfig.java
@@ -12,7 +12,7 @@ public class WebMvcConfig implements WebMvcConfigurer  {
 
 		@Override
 		public void addCorsMappings(CorsRegistry registry) {
-			registry.addMapping("/**").allowedOrigins("localhost:8080").allowedMethods("GET", "POST", "PUT", "DELETE");
+			registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST", "PUT", "DELETE").allowedHeaders("*");
 			
 		}
 
